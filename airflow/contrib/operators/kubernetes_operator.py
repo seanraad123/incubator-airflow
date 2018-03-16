f2dc32dbf403a20b497e6788e04bd604138a7cba
Jessica Laughlin <jessica.laughlin@bluecore.com>
2018-03-16 12:58:01 -0400

    all comments addressed

--- airflow/contrib/operators/kubernetes_operator.py
+++ airflow/contrib/operators/kubernetes_operator.py
@@ -3,8 +3,47 @@ import jinja2
 import logging
 import re
 import subprocess
+import tempfile
 import time
 import uuid
+
+YAML_TEMPLATE = """
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: {{ job_name }}
+spec:
+  template:
+    spec:
+      containers: {% for container in containers %}
+      - name: {{ container.name }}
+        image: {{ container.image }}
+        command: {{ container.command }} {% endfor %}
+      restartPolicy: Never
+  backoffLimit: 0
+"""
+
+
+def generate_yaml(unique_job_name, kubernetes_container_information_list):
+    """
+    Generate yaml string from YAML_TEMPLATE and inputs.
+    """
+    template = jinja2.Template(YAML_TEMPLATE)
+    return template.render(job_name=unique_job_name, containers=kubernetes_container_information_list)
+
+
+class KubernetesContainerInformation():
+    """
+    Information for an individual container,
+    used to generate Kubernetes Job yamls.
+    """
+    def __init__(self,
+                 name,
+                 image,
+                 command):
+        self.name = name
+        self.image = image
+        self.command = command
 
 # Amount of time to sleep between polling for the job status
 SLEEP_TIME_BETWEEN_POLLING = 60
@@ -40,22 +79,21 @@ class KubernetesJobOperator(BaseOperator):
         result = subprocess.check_output(args=args, stdin=stdin)
         logging.info(result)
 
-    def clean_up(self):
+    def clean_up(self, error_message=None):
         """
         Deleting the job removes the job and all related pods.
-        Cleans old yamls off Airflow worker.
+        Iff error_message, raise Exception.
         """
         self.run_subprocess(args=['kubectl', 'delete', 'job', self.unique_job_name])
-        # TODO: unlink was throwing errors, will investigate
-        self.run_subprocess(args=['rm', self.unique_filename])
+        if error_message:
+            raise Exception(error_message)
 
     def on_kill(self):
         """
         Run clean up.
         Raising an error will fail the KubernetesJobOperator task.
         """
-        self.clean_up()
-        raise Exception('Job %s was killed.' % self.unique_job_name)
+        self.clean_up(error_message='Job %s was killed.' % self.unique_job_name)
 
     def poll_job_completion(self):
         """
@@ -65,7 +103,7 @@ class KubernetesJobOperator(BaseOperator):
         logging.info('Polling for completion of job: %s' % self.unique_job_name)
         running_job_count = 1
         while running_job_count > 0:
-            job_description = subprocess.check_output(args=['kubectl', 'describe', 'job', self.unique_job_name])
+            job_description = self.run_subprocess(args=['kubectl', 'describe', 'job', self.unique_job_name])
             matched = re.search(r'(\d+) Running / \d+ Succeeded / (\d+) Failed', job_description)
             logging.info('Current status is: %s' % matched.group(0))
 
@@ -73,63 +111,19 @@ class KubernetesJobOperator(BaseOperator):
             failed_job_count = int(matched.group(2))
             # If any Jobs fail, fail KubernetesJobOperator task
             if failed_job_count != 0:
-                self.clean_up()
-                raise Exception('%s has failed pods, failing task.' % self.unique_job_name)
+                return '%s has failed pods, failing task.' % self.unique_job_name
 
             time.sleep(SLEEP_TIME_BETWEEN_POLLING)
 
     def execute(self, context):
         self.unique_job_name = '%s-%s' % (self.job_name, uuid.uuid4())
-        self.unique_filename = '%s.yaml' % self.unique_job_name
 
-        generate_yaml_and_write_file(self.unique_filename,
-                                     self.unique_job_name,
-                                     self.kubernetes_container_information_list)
+        yaml = generate_yaml(self.unique_job_name,
+                             self.kubernetes_container_information_list)
+        with tempfile.NamedTemporaryFile(suffix='.yaml') as f:
+            f.write(yaml)
+            f.flush()
+            self.run_subprocess(args=['kubectl', 'apply', '-f', f.name])
 
-        self.run_subprocess(args=['kubectl', 'apply', '-f', '%s' % self.unique_filename])
-
-        self.poll_job_completion()
-
-        self.clean_up()
-
-
-YAML_TEMPLATE = """
-apiVersion: batch/v1
-kind: Job
-metadata:
-  name: {{ job_name }}
-spec:
-  template:
-    spec:
-      containers: {% for container in containers %}
-      - name: {{ container.name }}
-        image: {{ container.image }}
-        command: {{ container.command }} {% endfor %}
-      restartPolicy: Never
-  backoffLimit: 0
-"""
-
-
-def generate_yaml_and_write_file(filename, unique_job_name, kubernetes_container_information_list):
-    """
-    Generate yaml string from YAML_TEMPLATE and inputs.
-    """
-    template = jinja2.Template(YAML_TEMPLATE)
-    yaml = template.render(job_name=unique_job_name, containers=kubernetes_container_information_list)
-    logging.info('Writing yaml file to Airflow worker: %s' % yaml)
-    with open(filename, 'w') as f:
-        f.write(yaml)
-
-
-class KubernetesContainerInformation():
-    """
-    Information for an individual container,
-    used to generate Kubernetes Job yamls.
-    """
-    def __init__(self,
-                 name,
-                 image,
-                 command):
-        self.name = name
-        self.image = image
-        self.command = command
+        message = self.poll_job_completion()
+        self.clean_up(error_message=message)
