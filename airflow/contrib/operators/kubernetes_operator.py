8492ddf3ffdc31d1dfde79e599d06d8d4f8130f6
Jessica Laughlin <jessica.laughlin@bluecore.com>
2018-03-30 13:03:41 -0400

    fix exceptions

--- airflow/contrib/operators/kubernetes_operator.py
+++ airflow/contrib/operators/kubernetes_operator.py
@@ -8,7 +8,7 @@ import time
 import uuid
 
 # Amount of time to sleep between polling for the job status
-SLEEP_TIME_BETWEEN_POLLING = 60
+SLEEP_SECONDS_BETWEEN_POLLING = 60
 
 
 class KubernetesJobOperator(BaseOperator):
@@ -39,22 +39,21 @@ class KubernetesJobOperator(BaseOperator):
         self.kubernetes_job_yaml_dictionary = kubernetes_job_yaml_dictionary or {}
         self.kubernetes_job_yaml_template = kubernetes_job_yaml_template
 
-    def clean_up(self, error_message=None):
+    def clean_up(self):
         """
         Deleting the job removes the job and all related pods.
         Iff error_message, raise Exception.
         """
         result = subprocess.check_output(args=['kubectl', 'delete', 'job', self.unique_job_name])
         logging.info(result)
-        if error_message:
-            raise Exception(error_message)
 
     def on_kill(self):
         """
         Run clean up.
         Raising an error will fail the KubernetesJobOperator task.
         """
-        self.clean_up(error_message='Job %s was killed.' % self.unique_job_name)
+        self.clean_up()
+        raise Exception('Job %s was killed.' % self.unique_job_name)
 
     def poll_job_completion(self):
         """
@@ -64,6 +63,8 @@ class KubernetesJobOperator(BaseOperator):
         logging.info('Polling for completion of job: %s' % self.unique_job_name)
         running_job_count = 1
         while running_job_count > 0:
+            time.sleep(SLEEP_SECONDS_BETWEEN_POLLING)
+
             job_description = subprocess.check_output(args=['kubectl', 'describe', 'job', self.unique_job_name])
             matched = re.search(r'(\d+) Running / \d+ Succeeded / (\d+) Failed', job_description)
             logging.info('Current status is: %s' % matched.group(0))
@@ -72,9 +73,7 @@ class KubernetesJobOperator(BaseOperator):
             failed_job_count = int(matched.group(2))
             # If any Jobs fail, fail KubernetesJobOperator task
             if failed_job_count != 0:
-                return '%s has failed pods, failing task.' % self.unique_job_name
-
-            time.sleep(SLEEP_TIME_BETWEEN_POLLING)
+                raise Exception('%s has failed pods, failing task.' % self.unique_job_name)
 
     def execute(self, context):
         self.unique_job_name = '%s-%s' % (self.kubernetes_job_name, uuid.uuid4())
@@ -89,5 +88,9 @@ class KubernetesJobOperator(BaseOperator):
             result = subprocess.check_output(args=['kubectl', 'apply', '-f', f.name])
             logging.info(result)
 
-        message = self.poll_job_completion()
-        self.clean_up(error_message=message)
+        try:
+            self.poll_job_completion()
+        except Exception as e:
+            raise e
+        finally:
+            self.clean_up()
